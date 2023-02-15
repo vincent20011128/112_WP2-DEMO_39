4eb95c23de2dd4e66db0718a15815cf445d1660c
vincent20011128 <210410139@gms.tku.edu.tw>
2023-02-15 21:59:25 +0800

    W01-P3: connect supabase from pgAdmin 4, and add 4 more data

---
+++ demo/w01/src/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO cart_39 (id,title,price,img,amount)
+VALUES
+(5,'Samsung Galaxy A53',399.99,'https://khrbtxhvkdftbumahmyi.supabase.co/storage/v1/object/public/demo-39/cart-39/Galaxy%20S8.png',1),
+(6,'google pixel 7',499.99,'https://khrbtxhvkdftbumahmyi.supabase.co/storage/v1/object/public/demo-39/cart-39/Google%20Pixel.png',1),
+(7,'Xiaomi 13' ,699.99,'https://khrbtxhvkdftbumahmyi.supabase.co/storage/v1/object/public/demo-39/cart-39/Xiaomi%20Redmi%20Note%202.png',1),
+(8,'Samsung Galaxy S23',599.99 ,'https://khrbtxhvkdftbumahmyi.supabase.co/storage/v1/object/public/demo-39/cart-39/Galaxy%20S7.png',1)
+
+
