e1c56792943fba07fbb562f595e88322473686f3
vincent20011128 <210410139@gms.tku.edu.tw>
2023-02-22 21:49:55 +0800

    w02

---
+++ demo/w02/card_demo_xx/utils/database.js
@@ -0,0 +1,13 @@
+const {Pool} = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    port: '5432',
+    database: 'wp2_demo_39',
+    password: '0000'
+});
+
+console.log('connect database',pool.options.database);
+
+module.exports = pool;
